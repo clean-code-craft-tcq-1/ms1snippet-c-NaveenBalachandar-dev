f601ae9b4ec9a9c0bb97711684f49a8c3ed7288a
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-03-25 19:34:30 +0530

    Update sensor-validate.h
    header adaption

--- sensor-validate.h
+++ sensor-validate.h
@@ -8,7 +8,7 @@
 /*Common Functions */
 /*Func to monitor sensor values range */
 int checkAbnormality_i(double value, double nextValue, double maxDelta);
-/*Func to read the sensor parametres like SOC ,current*/
+/*Funcs to validate the sensor parametres like SOC ,current*/
 int validateSensorParmtReadings_i(double* values, int numOfValues,double maxThreshold);
-
+int performIndvSensorvalid_i(double* values, int sensorLastIndex ,double maxThreshold);
 
