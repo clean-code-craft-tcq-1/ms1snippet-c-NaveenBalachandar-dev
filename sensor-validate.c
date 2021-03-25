4d30c5397f6318738197991a94103cb64ee27705
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-03-25 16:06:06 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -1,6 +1,14 @@
+/*****************************************************************************/
+/***    \file        sensor-validate.c
+***     \author      Naveen Balachandar (RBEI/ECK3) 
+***
+***     \brief       File to validate the sensors 
+/*****************************************************************************/
+
+/*Project headers*/
 #include "sensor-validate.h"
 
-int _give_me_a_good_name(double value, double nextValue, double maxDelta) {
+int rangeMonitor_i(double value, double nextValue, double maxDelta) {
   if(nextValue - value > maxDelta) {
     return 0;
   }
@@ -10,7 +18,7 @@ int _give_me_a_good_name(double value, double nextValue, double maxDelta) {
 int validateSOCreadings(double* values, int numOfValues) {
   int lastButOneIndex = numOfValues - 1;
   for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
+    if(!rangeMonitor_i(values[i], values[i + 1], 0.05)) {
       return 0;
     }
   }
@@ -20,7 +28,7 @@ int validateSOCreadings(double* values, int numOfValues) {
 int validateCurrentreadings(double* values, int numOfValues) {
   int lastButOneIndex = numOfValues - 1;
   for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
+    if(!rangeMonitor_i(values[i], values[i + 1], 0.1)) {
       return 0;
     }
   }
